522fa9ecd36035ef131c75baa88e0579341d0b91
shaotingcui382-droid <shaotingcui382@gmail.com>
2026-04-09 16:47:40 +0800

    mineru

--- docs/chapter5/code/pdfppl.py
+++ docs/chapter5/code/pdfppl.py
@@ -68,7 +68,7 @@ def generate_qa_from_pdfs(model):
 
     ppl = build_pdf2qa_pipeline(
         model=model,
-        mineru_api='http://10.119.23.139:20234',
+        mineru_api='http://10.119.23.139:20234',  # 需要填入你自己的服务地址
         image_output_folder=IMAGE_OUTPUT_FOLDER,
         chunk_key='chunk',
         image_key='image_path',
